c6534446d423675eaf9bbc7336f67ac504d37e64
Ilyuh_dr01@top-academy.ru <Ilyuh_dr01@top-academy.ru>
2025-08-26 14:40:28 -0400

    db.json

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -6,17 +6,18 @@ function Header() {
         <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
             <h1>{title}</h1>
-            <p>
+            <h2>JSON - JavaScript Object Notation</h2>
+            {/* <p>
                 Edit <code>src/App.js</code> and save to reload.
-            </p>
-            <a
+            </p> */}
+            {/* <a
                 className="App-link"
                 href="https://reactjs.org"
                 target="_blank"
                 rel="noopener noreferrer"
             >
                 Learn React
-            </a>
+            </a> */}
         </header>
     )
 }
